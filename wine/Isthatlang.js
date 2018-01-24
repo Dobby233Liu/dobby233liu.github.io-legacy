20afe7a9678d7132e72dd7c3993b7998215eac94
Liu-Wen-Yuan <15816141883@163.com>
2018-01-24 21:17:15 +0800

    Update Isthatlang.js

--- wine/Isthatlang.js
+++ wine/Isthatlang.js
@@ -12,5 +12,19 @@ return navigator.userLanguage = langCode;
 alert("isThatLang int err: " + e.message);
 }}}catch(e){
 alert(e.message)}
+    function isThatLang(){
+    try{
+if (!langCode){
+alert('isThatLang err: the "langCode" is not defined');
+console.log('isThatLang: lang code not defined');
+// throw "lang code undefined"
+return undefined;
+
+}else{
+return navigator.userLanguage = langCode;
+}catch(e){
+alert("isThatLang int err: " + e.message);
+}
+    }
 
 
