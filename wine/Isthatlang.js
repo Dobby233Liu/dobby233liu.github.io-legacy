9b348d68352a2ad1e48f7f27cb6c90f5ae0658bd
Dobby233Liu <15816141883@qq.com>
2018-01-24 21:35:28 +0800

    Commit with AIDE

--- wine/Isthatlang.js
+++ wine/Isthatlang.js
@@ -1,13 +1,14 @@
-try{this.isThatLang = function(langCode){
+try{
+	this.isThatLang = function(langCode){
 try{
 if (!langCode){
 alert('isThatLang err: the "langCode" is not defined');
 console.log('isThatLang: lang code not defined');
 // throw "lang code undefined"
 return undefined;
-
 }else{
 return navigator.userLanguage = langCode;
+}
 }catch(e){
 alert("isThatLang int err: " + e.message);
 }}}catch(e){
@@ -22,9 +23,10 @@ return undefined;
 
 }else{
 return navigator.userLanguage = langCode;
+}
 }catch(e){
 alert("isThatLang int err: " + e.message);
 }
-    }
+    }}}
 
 
