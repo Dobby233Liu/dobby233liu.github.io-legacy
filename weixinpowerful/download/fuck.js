132fb18dc6b75d4b4976e4c5fd3543c114342361
Liu-Wen-Yuan <15816141883@163.com>
2018-02-02 06:23:03 -0600

    Add files via upload

--- weixinpowerful/download/fuck.js
+++ weixinpowerful/download/fuck.js
@@ -2,15 +2,15 @@
 function cb (json) {
             location.href=json.url;
                 }
-function DlAlphaVersion(){try{
-  $.get("http://dldir1.qq.com/weixin/android/weixin_android_alpha_config.json?t="+~new Date,function(data,status){
-    if(status==success){
-eval(data);
-}
-  });}catch(e){
-alert("错误: "+e.message+" 请开一个issues");
-}
-}
+function DlAlphaVersion(){
+ 
+$.ajax({
+  url: "http://dldir1.qq.com/weixin/android/weixin_android_alpha_config.json?t=",
+  
+  success: function(result){
+    
+eval(result);
+}});
 function DlReleaseVersion(){
 try{
 location.href="http://weixin.qq.com/cgi-bin/download302?check=false&uin=&stype=&fr=&lang=zh_CN&url=android16";
